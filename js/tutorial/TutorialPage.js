f7b2a686fd15a33716889084f7ad50ce917c2e2c
Ian Strachan <ian@overlap.se>
2015-10-19 19:07:59 +0100

    Allow moving through instructions

--- js/tutorial/TutorialPage.js
+++ js/tutorial/TutorialPage.js
@@ -6,4 +6,36 @@ tutorial.TutorialPage = function (data) {
   self.instructions = ko.observableArray(data ? data.instructions : []);
   self.markUp = ko.observable(data ? data.markUp : '');
   self.script = ko.observable(data ? data.script : '');
+  var instructionIndex = ko.observable(0);
+
+  // computeds
+  self.canMoveToNextInstruction = ko.computed(function () {
+    if (!self.instructions()) {
+      return false;
+    }
+    return instructionIndex() + 1 < self.instructions().length;
+  });
+
+  self.canMoveToPreviousInstruction = ko.computed(function () {
+    if (!self.instructions()) {
+      return false;
+    }
+    return instructionIndex() > 0;
+  });
+
+  self.selectedInstruction = ko.computed(function () {
+    if (self.instructions() && instructionIndex() >= 0 && instructionIndex() < self.instructions().length) {
+      return self.instructions()[instructionIndex()];
+    }
+    return null;
+  });
+
+  // functions
+  self.moveToPreviousInstruction = function () {
+    instructionIndex(Math.max(0, instructionIndex() - 1));
+  };
+
+  self.moveToNextInstruction = function () {
+    instructionIndex(Math.min(self.instructions().length - 1, instructionIndex() + 1));
+  };
 };
