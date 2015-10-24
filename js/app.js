3f6ca1eff139622480dddf4aea4d3d002d285a56
Ian Strachan <ian@overlap.se>
2015-10-24 20:03:35 +0100

    Bit of extra styling and better sample data

--- js/app.js
+++ js/app.js
@@ -9,7 +9,9 @@ requirejs.config({
 require(['jquery', 'knockout', 'app/TutorialViewModel'], function($, ko, TutorialViewModel) {
   var data = {
     pages: [
-      {name: 'Page1', instructions: ['instructions 1.1', 'instructions 1.2'], markUp: '<div>test</div>', script: 'var id = 7;'},
+      {name: 'Tutorial: Introduction', instructions: ['instructions 1.1', 'instructions 1.2'],
+      markUp: '<p>First name: <strong>todo</strong></p><p>Last name: <strong>todo</strong></p>',
+      script: 'function AppViewModel() {this.firstName = "Bert";this.lastName = "Bertington";}ko.applyBindings(new AppViewModel());'},
       {name: 'Page2', instructions: ['instructions 2'], markUp: 'markUp 2', script: 'script 2'},
       {name: 'Page3', instructions: ['instructions 3'], markUp: 'markUp 3', script: 'script 3'}]
   };
